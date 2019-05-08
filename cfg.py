ae6798409743d22e6245317219b3d57075fe8937
Jonas Keturka <jk@beeone.eu>
2019-05-08 19:27:18 +0200

    Move db file to project dir

--- cfg.py
+++ cfg.py
@@ -5,6 +5,6 @@ from event_bus import EventBus
 
 __all__ = ['db', 'console']
 bus = EventBus()
-db_location = os.path.expanduser('~/data/tableau.db')
+db_location = 'data/tableau.db'
 
 print("DB Location: " + db_location)
